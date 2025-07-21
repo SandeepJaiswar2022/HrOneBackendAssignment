5dcf8d86a943eaf62621a2ce8b88ad0a3098c973
SandeepJaiswar2022 <sandy2662001@gmail.com>
2025-07-21 16:01:05 +0530

    Implemented defualt route

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status
 from routes.product_routes import router as product_router
 from routes.order_routes import router as order_router
 
 app = FastAPI()
+
+@app.get("/", status_code=status.HTTP_200_OK)
+def root():
+    return {"message": "Welcome to the E-commerce FAST_API! Append Order or Product to the URL to access respective endpoints."}
 
 # Routes
 app.include_router(product_router) # product router
